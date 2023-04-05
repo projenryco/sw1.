ebe5d130106f1972782ea1b4edffc04914527fbd
projetoenryco <100374841+projenryco@users.noreply.github.com>
2023-04-05 14:18:31 -0300

    Add files via upload

---
+++ conta bancaria/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace conta_bancaria;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("exercicio conta bancaria");
+        Conta conta = new Conta();
+        conta.nomeCliente = "enryco";
+        conta.limite = 500;
+
+        conta.depositar(200);
+        conta.sacar(50);
+        double saldo = conta.ConsultaSaldo();
+
+        Console.WriteLine("seu saldo é de: " + saldo);
+    }
+}
