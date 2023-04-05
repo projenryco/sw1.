254337c78253afe5566c24428aa381a9a5caefed
projetoenryco <100374841+projenryco@users.noreply.github.com>
2023-04-05 16:35:30 -0300

    Update Program.cs

--- aluno media/Program.cs
+++ aluno media/Program.cs
@@ -3,6 +3,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine();
+       Alunos aluno1 = new Alunos();
+        aluno1.nome = "outro aluno";
+        aluno1.nota1 = 4;
+        aluno1.nota2 = 5;
+        
+        aluno1.mensagem();
     }
 }
